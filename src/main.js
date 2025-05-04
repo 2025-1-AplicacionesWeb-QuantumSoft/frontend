de7c481e58f67559a4d4f8266e4793d7ec216361
LynnMeza <lynn770450@gmail.com>
2025-05-04 02:20:22 -0500

    feat: agregate components main js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,31 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { AutoComplete, Button, Card, CascadeSelect, Checkbox, ConfirmDialog, DataTable, DatePicker, FileUpload, FloatLabel, IconField, Image, InputIcon , Menu, Menubar, Message, Stepper, Textarea, ToggleSwitch, Toolbar, Tooltip } from 'primevue'
 
-createApp(App).mount('#app')
+const app= createApp(App)
+
+app
+    .component("pv-menu",Menu)
+    .component("pv-menubar",Menubar)
+    .component("pv-upload",FileUpload)
+    .component("pv-tooltip",Tooltip)
+    .component("pv-toolbar",Toolbar)
+    .component("pv-stepper",Stepper)
+    .component("pv-confirmDialog",ConfirmDialog)
+    .component("pv-card",Card)
+    .component("pv-autoComplete",AutoComplete)
+    .component("pv-cascadeSelect",CascadeSelect)
+    .component("pv-checkbox",Checkbox)
+    .component("pv-datePicker",DatePicker)
+    .component("pv-floatLabel",FloatLabel)
+    .component("pv-iconField",IconField)
+    .component("pv-textarea",Textarea)
+    .component("pv-toggleSwitch",ToggleSwitch)
+    .component("pv-button",Button)
+    .component("pv-dataTable",DataTable)
+    .component("pv-message",Message)
+    .component("pv-inputIcon",InputIcon)
+    .component("pv-image",Image)
+    .use(i18n)
+    .mount('#app')
