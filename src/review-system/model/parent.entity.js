490c1e797796518223acd44e78104d1e0cb8ccd7
JoaquinC <u20201f788@upc.edu.pe>
2025-05-06 21:40:23 -0500

    Agregado el bounded context de reseñas - 06/05/2025

---
+++ src/review-system/model/parent.entity.js
@@ -0,0 +1,10 @@
+export class Parent {
+    constructor(id, user_id, address, city, children_count, preferences) {
+        this.id = id;
+        this.user_id = user_id;
+        this.address = address;
+        this.city = city;
+        this.children_count = children_count;
+        this.preferences = preferences;
+    }
+}
