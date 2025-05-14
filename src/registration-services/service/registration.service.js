f54e4cbd5f8238eb1db09ba8d601683e3b953a53
Walter <u202221632@upc.edu.pe>
2025-05-14 13:14:45 -0500

    feat: change3

--- src/registration-services/service/registration.service.js
+++ src/registration-services/service/registration.service.js
@@ -1,6 +1,14 @@
-import axios from 'axios'
+import axios from 'axios';
+import {BabysitterAssembler} from './registration.assembler.js';
 
-const API_BASE = 'http://localhost:3001'
+const API_BASE = 'http://localhost:3001';
 
-export const getBabysitters = () => axios.get(`${API_BASE}/babysitter`)
-
+export const getBabysitters = async () => {
+    try {
+        const response = await axios.get(`${API_BASE}/babysitter`);
+        return BabysitterAssembler.toEntitiesFromResponse(response);
+    } catch (error) {
+        console.error("Error al obtener babysitters:", error);
+        return [];
+    }
+};
